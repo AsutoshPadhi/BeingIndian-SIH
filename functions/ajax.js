b475c8e82992ea0a6d96670d6cb40c25b4030117
juvekaradheesh <juvekaradheesh@gmail.com>
2017-03-22 13:03:14 +0530

    updated template

---
+++ functions/ajax.js
@@ -0,0 +1,22 @@
+function loadDoc(url) 
+{   
+    if (window.XMLHttpRequest)
+    {
+        // code for modern browsers
+        xhttp = new XMLHttpRequest();
+    }
+    else
+    {
+        // code for IE6, IE5
+        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xhttp.onreadystatechange = function()
+    {
+        if (this.readyState == 4 && this.status == 200)
+        {
+            document.getElementById("field").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
