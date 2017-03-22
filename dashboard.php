649cf3b2dd3c66f1a492b5f1c2c8227545d062ef
Dipshi <2015dipshi.shetty@ves.ac.in>
2017-03-22 15:42:11 +0530

    test

--- dashboard.php
+++ dashboard.php
@@ -131,7 +131,7 @@
                 <div class="sidebar-nav navbar-collapse">
                     <ul class="nav" id="side-menu">
                         <li>
-                            <a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                            <a onClick="javascript:loadDoc('login.php')"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
                         </li>
                         <li>
                             <a href="tables.html"><i class="fa fa-plus fa-fw"></i> Add Issue</a>
