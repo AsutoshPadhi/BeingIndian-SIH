af8ea9a84f14d7067967840e743205bca6330a0c
juvekaradheesh <juvekaradheesh@gmail.com>
2017-03-22 13:23:33 +0530

    none

--- dashboard.php
+++ dashboard.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
+    <!-- AJAX change page -->
     <script src="functions/ajax.js"></script>
 
     <title>Better India!</title>
