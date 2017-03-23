ff922843f45a5092bbdc9545118a7f2b5ef87229
Asutosh Padhi <2015asutosh.padhi@ves.ac.in>
2017-03-23 19:54:09 +0530

    Update

--- user/insertIssue.php
+++ user/insertIssue.php
@@ -1,36 +1,42 @@
 <?php
 
-	include 'functions/dataBaseConn.php';
+	include 'C:\xampp\htdocs\Github\testProject\functions\dataBaseConn.php';
 
 	session_start();
 	$state = $_GET['state'];
+	echo "state = ".$state;
 	$district = $_GET['district'];
 	$locality = $_GET['locality'];
 	$pin = $_GET['pin'];
-	$title = $_GET['title'];
+	$title = $_GET['issueTitle'];
 	$description = $_GET['description'];
 
-	$get_state_id = "SELECT state_id from state where state_name='.$state.'";
+	$district_id = 2;
+	$user_id = 2000;
+
+	#$get_state_id = "SELECT state_id from state where state_name='.$state.'";
 	$get_district_id = "SELECT district_id FROM district, state WHERE state.state_id = district.district_id";	
 	#Use nested or join
 
 	$get_last_issue_id = "SELECT issue_id FROM issue";
 
-	$result = conn->query($get_last_issue_id);
+	#to get the last issue_id
+	$result = $conn->query($get_last_issue_id);
+	$j=0;
 	if($result->num_rows > 0)
 	{
 		while ($row = $result->fetch_assoc())
 		{
-			#iterate till the last issue to get its id
-			$last_issue_id = $row['issue_id'];
+			$j++;
 		}
 	}
+	$issue_id = 100000+$j;
 
-	$issue_id = $last_issue_id+1;		//issue_id for the new issue
+	echo "issue_id".$issue_id;
 
 	#get region_id also
-	$get_region_id = "SELECT region_id FROM districtsinregion WHERE district_id = ".$district_id;
-	$result = conn->query($get_region_id);
+	$get_region_id = "SELECT region_id FROM districtsinregion WHERE district_id = 2";	//Change this number
+	$result = $conn->query($get_region_id);
 	if($result->num_rows > 0)
 	{
 		while ($row = $result->fetch_assoc())
@@ -39,6 +45,12 @@
 			$region_id = $row['region_id'];
 		}
 	}
+	//echo "region_id".$region_id;
+
+	$sql = "INSERT INTO issue(issue_id, user_id, district_id, region_id, title, description) VALUES
+			('$issue_id','$user_id','$district_id','$region_id','$title','$description')";
+	$conn->query($sql);
+	
 
 	//$sql = "INSERT INTO issue(issue_id,)";
 	if(isset($locality))
--- user/urlGenerator.js
+++ user/urlGenerator.js
@@ -6,7 +6,7 @@ function generateUrl(state,district,locality,pin,issueTitle)
 
 function generateUrlAdd(state,district,locality,pin,issueTitle,description)
 {
-    var url = "insertIssue.php?issue="+issueTitle+"&state="+state+"&district="+district+"&locality="+locality+"&pin="+pin+"&description="+description+"&callFunction=get_query";
+    var url = "insertIssue.php?issueTitle="+issueTitle+"&state="+state+"&district="+district+"&locality="+locality+"&pin="+pin+"&description="+description+"&callFunction=get_query";
     loadDoc(url);
 }
 
