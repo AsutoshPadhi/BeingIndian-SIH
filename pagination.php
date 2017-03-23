0b517fb668a74ca70f2c507401594ac46a23b326
ishashetty <2015isha.shetty@ves.ac.in>
2017-03-23 11:47:12 +0530

    test

---
+++ pagination.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<title>W3.CSS</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/3/w3.css">
+<body>
+<div class="w3-container">
+
+<h2>Pagination</h2>
+
+<div class="w3-bar">
+  <a href="css.php" class="w3-button" id="i1" onclick="f()">1</a>
+  <a href="css1.php" class="w3-button" id="i2" onclick="f()">2</a>
+</div>
+<script>
+</script>
+</div>
+</body>
+</html>
