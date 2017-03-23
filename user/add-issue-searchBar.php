c8b3ee5cd780297f1896a981300e0de06d636b54
Asutosh Padhi <2015asutosh.padhi@ves.ac.in>
2017-03-23 21:50:39 +0530

    Still Working

--- user/add-issue-searchBar.php
+++ user/add-issue-searchBar.php
@@ -8,9 +8,35 @@
 	</div>
 
 	<div class="form-group col-xs-6 col-md-3">
-	    <select class="form-control" name="district" id="district">             
+	    <select class="form-control" name="district" id="district" onclick="javascript: getDistrict">             
 	        <option disabled selected>Districts</option>
-	        <?php include 'district_list.php';?>
+	        <script>
+	            function getDistrict()
+	            {
+	            	var state = document.getElementById('state').value;
+		            alert(state);
+		            if (window.XMLHttpRequest)
+		            {
+		                // code for modern browsers
+		                xhttp = new XMLHttpRequest();
+		            }
+		            else
+		            {
+		                // code for IE6, IE5
+		                xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+		            }
+		            
+		            xhttp.onreadystatechange = function()
+		            {
+		                if (this.readyState == 4 && this.status == 200)
+		                {
+		                    document.getElementById("district").innerHTML = this.responseText;
+		                }
+		            };
+		            xhttp.open("GET", "district_list.php?state = "+state, true);
+		            xhttp.send();
+	            }
+	        </script>
 	    </select>
 	</div>
 
