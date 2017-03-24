94f1e724a994295429ada4fff2b06f73b8829a58
ishashetty <2015isha.shetty@ves.ac.in>
2017-03-24 16:12:35 +0530

    tested

--- functions/queries.php
+++ functions/queries.php
@@ -3,10 +3,18 @@
 
 	//user
 
-	$upvotedByMe = "SELECT";
+	$votedByMe = 
 	SELECT issue.title FROM issue INNER JOIN issueupvote on issue.issue_id=issueupvote.issue_id WHERE issueupvote.user_id=$_SESSION['LOGIN_USER'];//Session name
-	$addedByMe = "SELECT";
-	$search = "SELECT";
+	$toupvote=
+	update issue set upvote_count=upvote_count+1 where issue_id=$row['issue'] title=$row['title'];
+	$addedByMe = 
+	INSERT into issue(issue_id,user_id,added_on,district_id,region_id,locality,pincode,title,description,upvote_couNT,bogus_count,duplicate_count,solution_count) values();
+	$search = select title from issue where title like '%".$_POST['']."%'";
+	$upvotedbyme=
+	select title from issue where
+
+
+
 
 
 	//institute
