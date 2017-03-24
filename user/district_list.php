2885370ae233e9512595b0d7f89dc06d0d5e9638
juvekaradheesh <juvekaradheesh@gmail.com>
2017-03-24 09:14:44 +0530

    Revert "District acc to state"
    This reverts commit b78ef1c4b4a74c8a4fc55859462b0900f6f7ecd0.

--- user/district_list.php
+++ user/district_list.php
@@ -3,11 +3,13 @@
 	include 'C:\xampp\htdocs\Github\testProject\functions\dataBaseConn.php';
 
 	$state = $_GET['state'];
-	
+	//echo "<option>".$state."</option>";
+
 	$sql1 = "SELECT state_id FROM state WHERE state_name = '".$state."'";
 	$sid = $conn->query($sql1);
-	$row1 = $sid->fetch_assoc();
-	$sql = "SELECT district_name FROM district WHERE state_id = '".$row1['state_id']."'";
+	$sql = "SELECT district_name FROM district WHERE state_name = '".$sid."'";
+	//$sql = "SELECT district_name FROM district INNER JOIN state ON state.state_name = '".$state"'";
+	//$sql = "SELECT district_name FROM district WHERE state_id IN (SELECT state_id FROM state WHERE state_name = '".$state."'");
 	$result = $conn->query($sql);
 	if($result->num_rows>0)
 	{
