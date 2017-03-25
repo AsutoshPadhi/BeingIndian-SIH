e08e50228e0f1e70c76fd57f1e9bb465ab952096
Asutosh Padhi <2015asutosh.padhi@ves.ac.in>
2017-03-25 09:28:51 +0530

    updated

--- user/add-issue-searchBar.php
+++ user/add-issue-searchBar.php
@@ -1,18 +1,17 @@
 <form>
 
-	<div class="form-group col-xs-6 col-md-3">
-	    <select class="form-control" name="state" id="state1" onchange="getDistrict(document.getElementById('state1').value)">
+	<div class="form-group col-xs-6 col-md-2">
+        <select class="form-control" id="state1" onchange="getDistrict(document.getElementById('state1').value);return false;">
             <option disabled selected>State</option>
-	        <?php include 'stateList.php'; ?>
-	    </select>
-	</div>
+            <?php include 'stateList.php'; ?>
+        </select>
+    </div>
 
-	<div class="form-group col-xs-6 col-md-3">
-	    <select class="form-control" name="district" id="district1">             
-	        <option disabled selected>Districts</option>
-	        <?php include 'district_list.php';?>
-	    </select>
-	</div>
+    <div class="form-group col-xs-6 col-md-2">
+        <select class="form-control" id="district1">             
+            <option disabled selected>Districts</option>
+        </select>
+    </div>
 
 	<div class="col-xs-6 col-md-3">
 		<input class="form-control" name="locality" id="locality" type="text" placeholder="locality (optional)">
