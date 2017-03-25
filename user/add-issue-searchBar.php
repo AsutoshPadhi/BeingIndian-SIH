7da95e548ea81f4a1607f16fab4e3cd2760c75be
Asutosh Padhi <2015asutosh.padhi@ves.ac.in>
2017-03-25 14:04:19 +0530

    Add Issue Completed

--- user/add-issue-searchBar.php
+++ user/add-issue-searchBar.php
@@ -1,33 +1,42 @@
-<form>
+<!DOCTYPE html>
+<html>
+    <head>
 
-	<div class="form-group col-xs-6 col-md-2">
-        <select class="form-control" id="state" onchange="getDistrict(document.getElementById('state').value);return false;">
-            <option disabled selected>State</option>
-            <?php include 'stateList.php'; ?>
-        </select>
-    </div>
+    </head>
 
-    <div class="form-group col-xs-6 col-md-2">
-        <select class="form-control" id="district">             
-            <option disabled selected>Districts</option>
-        </select>
-    </div>
+    <body>
+		<form>
 
-	<div class="col-xs-6 col-md-3">
-		<input class="form-control" name="locality" id="locality" type="text" placeholder="locality (optional)">
-	</div>
+			<div class="form-group col-xs-6 col-md-2">
+		        <select class="form-control" id="state2" onchange="getDistrict((document.getElementById('state2').value),'district2')">
+		            <option disabled selected>State</option>
+		            <?php include 'stateList.php'; ?>
+		        </select>
+		    </div>
 
-	<div class="col-xs-6 col-md-3">
-	    <input class="form-control" name="pin" id="pin" type="text" placeholder="pincode (optional)">
-	</div>
+		    <div class="form-group col-xs-6 col-md-2">
+		        <select class="form-control" id="district2">             
+		            <option disabled selected>Districts</option>
+		        </select>
+		    </div>
 
-	<div style="position: relative;top: 50%;">
-		<div class="form-group col-xs-6 col-md-9">
-		<input class="form-control" name="issueTitle" id="issueTitle" placeholder="What Issue are you facing?">
-		</div>
-		<div class="form-group col-xs-6 col-md-3">
-		<button type="button" class="btn btn-primary" onclick="generateUrl((document.getElementById('state').value),(document.getElementById('district').value),(document.getElementById('locality').value),(document.getElementById('pin').value),(document.getElementById('issueTitle').value))">Next</button>
-		</div>
-	</div>
+			<div class="col-xs-6 col-md-3">
+				<input class="form-control" name="locality" id="locality" type="text" placeholder="locality (optional)">
+			</div>
 
-</form>
+			<div class="col-xs-6 col-md-3">
+			    <input class="form-control" name="pin" id="pin" type="text" placeholder="pincode (optional)">
+			</div>
+
+			<div style="position: relative;top: 50%;">
+				<div class="form-group col-xs-6 col-md-9">
+				<input class="form-control" name="issueTitle" id="issueTitle" placeholder="What Issue are you facing?">
+				</div>
+				<div class="form-group col-xs-6 col-md-3">
+				<button type="button" class="btn btn-primary" onclick="generateUrl((document.getElementById('state2').value),(document.getElementById('district2').value),(document.getElementById('locality').value),(document.getElementById('pin').value),(document.getElementById('issueTitle').value))">Next</button>
+				</div>
+			</div>
+
+		</form>
+	</body>
+</html>
