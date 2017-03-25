a4bc31aa43f60634af158a2c7abe4fbd28765ba6
Dipshi <2015dipshi.shetty@ves.ac.in>
2017-03-25 13:12:53 +0530

    changed

--- functions/issueFeatures.php
+++ functions/issueFeatures.php
@@ -1,51 +1,165 @@
-<?php
-	
-	include('queries.php');
-	include('dataBaseConn.php');
-	
-	function issueStatus()
+<!--<?php
+
+	/*include '../functions/dataBaseConn.php';
+	function showStatus()
 	{
+		
+
 		$state_id = 1006;
 		$sql = "SELECT *FROM issue WHERE issue_id = .$state_id.";
 		$result = $conn->query($sql);
 		$row = $result->fetch_assoc();
 		echo "test";
-		echo "".$row['upvote_count'];
-		
-		
-		$sql = "SELECT * from issue where 1";
+		echo "".$row['upvote_count'];*/
+//	}
+
+?>-->
+
+
+<!doctype html>
+<html>
+<head>
+</head>
+<body>
+<?php
+
+	//show whether voting is closed or not
+	function showStatus()
+	{
+		include '../functions/dataBaseConn.php';
+
+		//$state_id = 1006;
+		$sql ="SELECT * FROM issue WHERE issue_id =100000";
 		$result = $conn->query($sql);
 		$row = $result->fetch_assoc();
-		if($row["upvote_count"]>=500)
-			{
-				return 0; //display voting closed 
-			}
-			else
-			{
-				return 1;//show upvote button
-			}
-		
-	}
-	
-	function collegeStatus()
-	{
-		$sql = "SELECT * from issuebogusupvote where inst_id=$_";
-		
-	}
-	
-	function userStatus()
-	{
-		//isha
-		$sql="select *from issue where issue_id =";
-		if($result==TRUE)
+		//show and disable upvote button
+		echo "<h1>Status</h1>";
+		/*if($row['upvote_count']>=500)
 		{
-			return 0;//you hav alraedy voted 
+			echo "voting close";
 		}
 		else
 		{
-			return 1;//upvote button
+			echo "upvote button";
+		}*/
+		echo "<br>";
+		//see solution button
+		if($row['solution_count']>0)
+		{
+		     echo "solution available";
 		}
+		else 
+		{
+			echo "solution Awaited";
+		}
+	}
+	function historyadd($sql)
+	{
+		include '../functions/dataBaseConn.php';
+		$result = $conn->query($sql);
+		//$row = $result->fetch_assoc();
+		if($result->num_rows!=0)
+		{
+    // output data of each row
 	
+			while($row = $result->fetch_assoc())
+				{
+
+					echo $row['title'];
+	
+				}
+		}
+		
+	}
+	function collegeStatus($variable)
+	{
+		include '../functions/dataBaseConn.php';
+		$sql="select * from issuebogusupvote where inst_id=' ' And isuue_id=  ";
+		$result = $conn->query($sql);
+		if($result==TRUE)
+		{
+		     return 1;	//status bogus
+		}
+		$sql="select * from issueduplicateupvoteupvote where inst_id=' ' And isuue_id=  ";
+		$result = $conn->query($sql);
+		if($result==TRUE)
+		{
+			return 2;  //status marked duplicate
+		}
+		
+	}
+	function userStatus($variable)
+	{
+		include '../functions/dataBaseConn.php';
+		$sql= $variable;//"select * from issueupvote where user_id=   ";//user session
+		$result = $conn->query($sql);
+		if($result==TRUE)
+		{
+			return 0;//already upvoted
+		}
+		else
+		{
+			return 1;//not upvoted
+		}
+		
 	}
 	
+	function status()
+	{
+		include '../functions/dataBaseConn.php';
+		//$state_id = 1006;
+		//$sql =$variable; 
+		$sql= "SELECT *FROM issue WHERE issue_id = .$state_id.";
+		$result = $conn->query($sql);
+		$row = $result->fetch_assoc();
+		if($row['uovote_count']>=500)
+		{
+			
+			if($row['approved_solution']>0)
+			{
+				echo "status approved";
+			}
+			else
+				if($row['solution_count']>0)
+				
+				{
+					echo " Following number of solutions are available";
+				}
+				else
+					if($row['dupicate_count']>1)
+					{
+						echo "";
+					}
+				else
+					if($row['bogus_count']>5)
+					{
+						return 4;
+					}
+				else
+					return 0;//solution awaited
+			
+				
+		}
+		else
+		{
+			return 5;//voting on
+		}
+	}
 ?>
+
+
+
+
+
+<?php
+showStatus();
+echo "<h1>Status</h1>";
+historyadd("select * from issue inner join user on issue.user_id=user.user_id where  user_email='shreyajangale@gmail.com'");
+
+?>
+
+
+
+
+</body>
+</html>
