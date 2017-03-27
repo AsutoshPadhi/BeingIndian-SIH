48b3b8a70bc3b08c03ba4a84131cf5ca6a256ae3
juvekaradheesh <juvekaradheesh@gmail.com>
2017-03-27 12:13:52 +0530

    user profile fixed

--- user/profile-update.php
+++ user/profile-update.php
@@ -1,12 +1,14 @@
 <?php
-	
+	session_start();
 	include '../functions/dataBaseConn.php';
 	//session_start();
-	$email = $_POST['email'];
+	$email = $_SESSION['$email'];
 	$mobile = $_POST['mobile'];
+	$fname = $_POST['fname'];
+	$lname = $_POST['lname'];
 	$state = $_POST['state3'];
 	$district = $_POST['district3'];
-	echo $state;
+
 
 	#query to get district_id
 	$get_district_id = "SELECT *FROM district WHERE district_name = '".$district."'";
@@ -15,15 +17,14 @@
 	$district_id = $row['district_id'];
 
 	#sql to insert the details into database
-	$sql = "UPDATE user SET mobile_number = '".$mobile."' WHERE user_email = '".$email."'";
+	$sql = "UPDATE user SET mobile_number='$mobile', fname='$fname', lname='$lname', district_id = $district_id WHERE user_email='$email'";
 	$result = $conn->query($sql);
-	$row = $result->fetch_assoc();
 	if($result)
 	{
-		echo 'true';
+		header('Location: dashboard.php');
 	}
 	else
 	{
-		echo 'error';
+		header('Location: logout.php');
 	}
 ?>
