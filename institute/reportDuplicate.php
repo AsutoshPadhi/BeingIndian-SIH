6dc5c135b6a183a60e2e7b98a65a53d89d6fb2f4
ishashetty <2015isha.shetty@ves.ac.in>
2017-04-02 07:31:04 +0530

    changes

--- institute/reportDuplicate.php
+++ institute/reportDuplicate.php
@@ -4,16 +4,21 @@
 <?php
 //duplicate issues
     session_start();
+	include '../functions/dataBaseConn.php';
     require('../functions/func_in.php');
     //require("issue-collapse.php");
-    
+    $r1=$_GET['url'];
     if(isset($_GET['inst'])&&$_GET['issue']){
         $inst_id = $_GET['inst'];
         $issue_id = $_GET['issue'];
     }
     if(isset($_GET['url']))//one of which is being passed through get method
 	{
+		
+	//	$r2=$GLOBALS['r1'];
+		//echo $r2;
         if(reportDuplicate($inst_id,$issue_id,$_GET['url'])){
+			
             echo "You've reported this issue as duplicate to ".$_GET['url']."";
             updateDuplicate($inst_id,$issue_id,$_GET['url']);
         }
@@ -26,17 +31,21 @@
         <label for="solutionUrl">Duplicate Issue ID</label>
         <br>
         <div class="col-md-6">
+		
 		<?php
-		$sql="SELECT *,count(inst_id)as colleges  FROM `issueduplicateupvote` group by issue_id,similar_to_issue having issue_id=$issue_id";
+		//echo $issue_id;
+		$sql="SELECT count(inst_id)as colleges  FROM `issueduplicateupvote` group by issue_id,similar_to_issue having issue_id=$issue_id";
 		$result=$conn->query($sql);
-		if($result)
-		{
+		while($row=$result->fetch_assoc()){
 			$var=$row['colleges'];
-			echo $var;
-		}
+            echo "<b><i>".$var." colleges have marked problem id ".$issue_id."  as duplicate of".$r1."</i></b><br>";
+		} 
+	
 		?>
+		<br>
         <input class="form-control" id="solutionUrl" name="solutionUrl" type="text" placeholder="Enter issue id of similar issue">
         </div>
+		
         <button type="submit" onclick="getUrl2(getElementById('solutionUrl').value,<?php echo $inst_id.",".$issue_id;?>)" class="btn btn-default">Report as Duplicate</button></div>
 <?php
 
