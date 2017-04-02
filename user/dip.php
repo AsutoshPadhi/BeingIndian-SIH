b9c37aa52ee3aaf96cbcf17ecb07f27f05da3131
Dipshi <2015dipshi.shetty@gmail.com>
2017-04-02 16:42:11 +0530

    upvote issue done

--- user/dip.php
+++ user/dip.php
@@ -1,11 +1,11 @@
 <!--upcount middle page for calling upvotecount function -->
 <?php
 	
-	$userid=$_GET['userid'];
+	$email=$_GET['userid'];
 	$issueid=$_GET['issueid'];
 	require '../functions/func_in.php';
 	include '../functions/dataBaseConn.php';
 	
-	upvotecount($issueid,$userid);
+	upvotecount($issueid,$email);
 	
 ?>
